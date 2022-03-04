2e90f67aeb3980639e03b6bfa9bcb40867a4d6c8
ErinaGarnisWidyawati <erinagarnis@gmail.com>
2022-03-04 10:13:29 +0700

    Initial commit

---
+++ app/src/main/java/www/smktelkommlg/sch/id/myviewandviewsgroup/MainActivity.kt
@@ -0,0 +1,16 @@
+package www.smktelkommlg.sch.id.myviewandviewsgroup
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.picodiploma.myviewandviews.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Iphone 13"
+
+    }
+}
